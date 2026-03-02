29c20d3a5838f987c842fd6ea65e21cead34cbb9
random44353241234 <sebastian.denbrok@web.de>
2026-03-02 15:55:46 -0500

    3a

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyse): # URL of the sentiment analysis service 
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -11,5 +12,26 @@ def emotion_detector(text_to_analyse): # URL of the sentiment analysis service
 
     # Sending a POST request to the sentiment analysis API 
     response = requests.post(url, json=myobj, headers=header)
-    
-    return response.json()
+
+    formatted_response = json.loads(response.text)
+    #print(formatted_response)
+
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+
+    anger_score = emotions['anger']
+    disgust_score = emotions['disgust']
+    fear_score = emotions['fear']
+    joy_score = emotions['joy']
+    sadness_score = emotions['sadness']
+
+    # Determine dominant emotion
+    dominant_emotion = max(emotions, key=emotions.get)
+
+    return {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
